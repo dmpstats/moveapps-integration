1d83ec8ccf9aa8f3ca967819ef04b475b2d0ed0b
leetalbott <lee.talbott@gmail.com>
2022-06-13 18:00:39 -0600

    Update RFunction.R
    add entire movestack data frame to event details
    print number of bactches posted successfully

--- RFunction.R
+++ RFunction.R
@@ -11,32 +11,26 @@ rFunction = function(server,api_key,batch_size,data) {
     batch_size <- 50
   }
   
+#  make.names(names(data@data),allow_=FALSE)
   num_batches <- ifelse(batch_size==0,1,ceiling(nrow(data)/batch_size)) # determine number of batches needed for outer loop based on number of observations in the movestack
+  
+  status_codes <- numeric() # for capturing API response status codes
   for (i in 1:num_batches)
   {
     if (batch_size==0) batch <- data else batch <- data[((i-1)*batch_size+1):(min(i*batch_size,nrow(data))),]
+    names(batch@data) <- gsub(".","_",names(batch@data),fixed=TRUE)
+    batch@data$moveapp_id <- moveapp_id
+    batch@data[is.na(batch@data)] <- ""
     for (j in 1:nrow(batch))
-    {
-      if ("clusterID" %in% names(batch)) # if the input movestack contains a cluster id field then output as a cluster event
       {
-        output <- list("device_id"=batch@data$tagID[j]
-                       ,"recorded_at"=format(batch@data$timestamp[j],"%Y-%m-%d %X%z")
-                       ,"location"=list("x"=batch@data$location.long[j],"y"=batch@data$location.lat[j])
-                       ,"title"=batch@study
-                       ,"event_type"="moveapps_cluster"
-                       ,"event_details"=list("cluster_id"=batch@data$clusterID[j],"clustered_points"=batch@data$n.locs[j],"clustered_ids"=batch@data$n.ids[j])
-        )
-      } else # else output a generic event
-      {
-        output <- list("device_id"=batch@data$tagID[j]
-                       ,"recorded_at"=format(batch@data$timestamp[j],"%Y-%m-%d %X%z")
-                       ,"location"=list("x"=batch@data$location_long[j],"y"=batch@data$location_lat[j])
-                       ,"title"=batch@study
-                       ,"event_type"="moveapps_other"
-                       ,"event_details"=list("dummy_detail_1"=1,"dummy_detail_2"=2)
-        )
-      }
-      output_json <- toJSON(output,pretty=TRUE,auto_unbox=TRUE) # convert list to json and add square brackets to conform to ER API expected json format
+      output <- list("device_id"=batch@data$tag_id[j]
+                     ,"recorded_at"=format(batch@data$timestamp[j],"%Y-%m-%d %X%z")
+                     ,"location"=list("x"=batch@data$location_long[j],"y"=batch@data$location_lat[j])
+                     ,"title"=batch@study
+                     ,"event_type"=event_type
+                     ,"event_details"=as.list(batch[j]@data)
+                     )
+      output_json <- toJSON(output,pretty=TRUE,auto_unbox=TRUE) # convert list to json
       output_json_str <- toString(output_json)
       if (j==1)
       {
@@ -47,16 +41,18 @@ rFunction = function(server,api_key,batch_size,data) {
       }
     }
     er_json <- paste0("[",all_output_json_str,"]") # convert list to json and add square brackets to conform to ER API expected json format
-    
+
     er_post <- POST(
       url = server,
       add_headers(apikey = api_key, 
                   "accept" = "application/json",
                   "content-type" = "application/json")
-      , body = er_json
-    )
+      ,body = er_json
+      )
+    status_codes[i] <- status_code(er_post)
   }
-  
+
+  logger.info(paste0(sum(status_codes==200)," of ",num_batches," batches posted successfully to EarthRanger"))
   result <- data
   return(result)
 }
