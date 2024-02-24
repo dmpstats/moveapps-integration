3d2a70ec646e8cf806e6aaf91fca586111434dc9
jeslefcourt <32883811+jeslefcourt@users.noreply.github.com>
2024-02-23 19:33:48 -0800

    Version 2, based on move2 object instead of movestack

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ MoveApps
 Github repository: https://github.com/PADAS/moveapps-integration
 
 ## Description
-Simple app to upload data from a movestack to EarthRanger via API.
+Simple app to upload data from a move2 object to EarthRanger via API.
 
 ## Documentation
-This app takes data from a movestack object, coerces to json via jsonlite packager, and iterates over the elements to push to EarthRanger via httr package. 
+This app takes data from a move2 object, coerces to json via jsonlite packager, and iterates over the elements to push to EarthRanger via httr package. 
 
 ### Input data
-moveStack in Movebank format
+move2 data
 
 ### Output data
-moveStack in Movebank format
+move2 data (passthrough)
 
-### Artefacts
+### Artifacts
 none.
 
 ### Parameters 
@@ -25,3 +25,4 @@ api_key:  EarthRanger api key
 batch_size:  number of records to push to EarthRanger at one time - batch_size of 0 causes all records to be pushed at once
 event_type:  description of event to store in EarthRanger system
 moveapp_id:  a user-supplied id to pass to EarthRanger
+eventlist_fields:  Additional parameters from the move2 object to pass to EarthRanger as part of the event_details
--- RFunction.R
+++ RFunction.R
@@ -1,40 +1,50 @@
-library('move')
+library('move2')
 library('jsonlite')
 library('httr')
 
-rFunction = function(server,api_key,batch_size,event_type,moveapp_id,eventlist_fields,data) {
-  Sys.setenv(tz="UTC")
+## The parameter "data" is reserved for the data object passed on from the previous app
 
+# to display messages to the user in the log file of the App in MoveApps
+# one can use the function from the logger.R file:
+# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+
+# Showcase injecting app setting (parameter `year`)
+rFunction = function(sdk, server,api_key,batch_size,event_type,moveapp_id,eventlist_fields,data, ...) {
+  logger.info(paste("Welcome to the", sdk))
+  
+  Sys.setenv(tz="UTC")
   if (is.null(batch_size))
   {
     logger.info("Your batch size is not supplied. We use default 50 observations per API call")
     batch_size <- 50
   }
   
-#  make.names(names(data@data),allow_=FALSE)
   num_batches <- ifelse(batch_size==0,1,ceiling(nrow(data)/batch_size)) # determine number of batches needed for outer loop based on number of observations in the movestack
+
   
   status_codes <- numeric() # for capturing API response status codes
   for (i in 1:num_batches)
   {
     if (batch_size==0) batch <- data else batch <- data[((i-1)*batch_size+1):(min(i*batch_size,nrow(data))),]
-    names(batch@data) <- gsub(".","_",names(batch@data),fixed=TRUE)
-    batch@data[is.na(batch@data)] <- ""
+    names(batch) <- gsub(".","_",names(batch),fixed=TRUE)
     eventlist_fields_parsed <- unlist(strsplit(eventlist_fields,",|;")) # parse requested event list field names into a vector (allows comma or semicolon)
     eventlist_fields_trimmed <- gsub("\\s+","",eventlist_fields_parsed) # remove spaces from parsed event list field names
-    eventlist_fields_filtered <- eventlist_fields_trimmed[eventlist_fields_trimmed %in% names(batch@data)] # remove requested event list field names that are not in the dataset
-    if (length(eventlist_fields_filtered)==0) eventlist_fields_filtered <- names(batch@data) # use all field names if no event list field names were supplied
+    eventlist_fields_filtered <- eventlist_fields_trimmed[eventlist_fields_trimmed %in% names(batch)] # remove requested event list field names that are not in the dataset
+    if (length(eventlist_fields_filtered)==0) eventlist_fields_filtered <- names(batch) # use all field names if no event list field names were supplied
+    
     for (j in 1:nrow(batch))
+    {
+      output <- list("device_id"=batch$tagID[j]
+                     ,"recorded_at"=format(batch$timestamp[j],"%Y-%m-%d %X%z")
+                     ,"location"=list("x"=batch$location_long[j],"y"=batch$location_lat[j]))
+      event_details = list()
+      for(field in eventlist_fields_filtered)
       {
-      output <- list("device_id"=batch@data$tagID[j]
-                     ,"recorded_at"=format(batch@data$timestamp[j],"%Y-%m-%d %X%z")
-                     ,"location"=list("x"=batch@data$location_long[j],"y"=batch@data$location_lat[j])
-                     ,"moveapp_id"=moveapp_id
-                     ,"title"=batch@study
-                     ,"event_type"=event_type
-                     ,"event_details"=as.list(batch[j]@data[eventlist_fields_filtered])
-                     )
-      output_json <- toJSON(output,pretty=TRUE,auto_unbox=TRUE) # convert list to json
+        event_details[field] <- batch[[field]][j]
+      }
+      output$event_details <- event_details
+    
+      output_json <- toJSON(output,pretty=FALSE,auto_unbox=TRUE) # convert list to json
       output_json_str <- toString(output_json)
       if (j==1)
       {
@@ -45,18 +55,30 @@ rFunction = function(server,api_key,batch_size,event_type,moveapp_id,eventlist_f
       }
     }
     er_json <- paste0("[",all_output_json_str,"]") # convert list to json and add square brackets to conform to ER API expected json format
-
+    url <- server
+    if(!grepl("\\?", server))
+    {
+      url <- paste0(url, "?")
+    }
+    else
+    {
+      url <- paste0(url, "&")  
+    }
+    url <- paste0(url, "event_type=", URLencode(event_type), "&moveapp_id=", URLencode(moveapp_id))
+    
+    logger.info(paste0("Posting data to ", url))
     er_post <- POST(
       url = server,
       add_headers(apikey = api_key, 
                   "accept" = "application/json",
                   "content-type" = "application/json")
       ,body = er_json
-      )
+    )
     status_codes[i] <- status_code(er_post)
   }
-
+  
   logger.info(paste0(sum(status_codes==200)," of ",num_batches," batches posted successfully to EarthRanger"))
   result <- data
   return(result)
+    
 }
